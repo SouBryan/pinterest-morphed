a9133f4bf6bfb41f48c76e2b04f63e460679081c
SouBrayn <110749727+SouBrayn@users.noreply.github.com>
2026-01-08 14:40:24 -0300

    Refactor settings.gradle.kts for dependency resolution

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,41 +1,16 @@
-rootProject.name = "morphe-patches-template"
-
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        google()
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/MorpheApp/registry")
-            credentials {
-                username = providers.gradleProperty("gpr.user").orNull ?: System.getenv("GITHUB_ACTOR")
-                password = providers.gradleProperty("gpr.key").orNull ?: System.getenv("GITHUB_TOKEN")
-            }
-        }
-        maven { url = uri("https://jitpack.io") }
-    }
-}
-
-plugins {
-    id("app.morphe.patches") version "1.0.0"
-}
-
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        mavenLocal()
         google()
-        maven { url = uri("https://jitpack.io") }
         mavenCentral()
+        maven { url = uri("https://jitpack.io") }
         maven {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/MorpheApp/registry")
             credentials {
-                username = providers.gradleProperty("gpr.user").orNull ?: System.getenv("GITHUB_ACTOR")
-                password = providers.gradleProperty("gpr.key").orNull ?: System.getenv("GITHUB_TOKEN")
+                username = settings.providers.gradleProperty("gpr.user").orNull ?: System.getenv("GITHUB_ACTOR")
+                password = settings.providers.gradleProperty("gpr.key").orNull ?: System.getenv("GITHUB_TOKEN")
             }
         }
     }
 }
-
-include(":extensions:extension")
