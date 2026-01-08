e36bb882112f6652f87d742c3f2c4bb7aa8fbe08
SouBrayn <110749727+SouBrayn@users.noreply.github.com>
2026-01-08 15:39:27 -0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,38 +1,15 @@
 pluginManagement {
     repositories {
-        google()
-        mavenCentral()
         gradlePluginPortal()
-        maven {
-            url = uri("https://maven.pkg.github.com/MorpheApp/registry")
-            credentials {
-                username = settings.providers.gradleProperty("gpr.user").orNull ?: System.getenv("GITHUB_ACTOR")
-                password = settings.providers.gradleProperty("gpr.key").orNull ?: System.getenv("GITHUB_TOKEN")
-            }
-        }
-    }
-}
-
-plugins {
-    id("app.morphe.patches") version "1.0.0"
-}
-
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
         google()
-        mavenCentral()
-        maven { url = uri("https://jitpack.io") }
         maven {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/MorpheApp/registry")
             credentials {
-                username = settings.providers.gradleProperty("gpr.user").orNull ?: System.getenv("GITHUB_ACTOR")
-                password = settings.providers.gradleProperty("gpr.key").orNull ?: System.getenv("GITHUB_TOKEN")
+                username = providers.gradleProperty("gpr.user").getOrNull() ?: System.getenv("GPR_USER")
+                password = providers.gradleProperty("gpr.key").getOrNull() ?: System.getenv("GPR_KEY")
             }
         }
+        maven { url = uri("https://jitpack.io") }
     }
 }
-
-rootProject.name = "pinterest-morphed"
-include(":patches", ":extensions")
